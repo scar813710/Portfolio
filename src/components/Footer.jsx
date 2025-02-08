69e2b5f9b8c4ca9e8a8270268b26c15a2cbe483a
scar813710 <rodolfguerr@gmail.com>
2025-02-09 00:43:21 +0900

    update home section text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="border-t border-[#d4d4d480]">
-      <p className="py-8 text-[#aaaaaa]">© Made with ❤️ and lots of ☕ from 2020 - 2025</p>
+      <p className="py-8 text-[#aaaaaa]">© Made with ❤️ and lots of ☕ from 2016 - 2025</p>
     </div>
   );
 };
